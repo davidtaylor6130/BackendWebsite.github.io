be12a2611f642219651010410b386811d0676c46
David Taylor <43450525+davidtaylor6130@users.noreply.github.com>
2021-12-06 12:46:11 +0000

    Hopefuly final fix

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,7 @@
 var portfolioJson;
 // Write Jacascript Here
 function LoadPortfolioWebsite(){
- LoadPortfolio("/Users/davidtaylormacbookpro/Documents/GitHub/Backend-for-Website-Text/Test1.json")
+ LoadPortfolio("/Users/davidtaylormacbookpro/Documents/GitHub/Backend-for-Website-Text/Test1.json");
 }
 
 function LoadPortfolio(SettingFileLocation) {
@@ -10,24 +10,27 @@ function LoadPortfolio(SettingFileLocation) {
 }
 
 function CreateHTMLProjectElements(){
+
+
  // Project number: 0
  //Element 0
  // Text Component
- var portolioElemet0 = document.getElementById("TEMP1 ");
- portolioElemet0.innerHTML = "Default Text";
+ var portolioElemet = document.getElementById("TEMP1 ");
+ portolioElemet.innerHTML = "Default Text";
 
  // Project number: 1
  //Element 0
  // Youtube Component
- var portolioElemet1 = document.getElementById("Temp2 ");
- portolioElemet0.src = "Default Text";
+ var portolioElemet = document.getElementById("Temp2 ");
+ portolioElemet.src = "Default Text";
 
  // Project number: 2
  //Element 0
  // Link Component
- var portolioElemet2 = document.getElementById("Temp3 ");
- portolioElemet0.href = "www.google.com";
+ var portolioElemet = document.getElementById("Temp3 ");
+ portolioElemet.href = "www.google.com";
 
  // Project number: 3
 
 }
+
